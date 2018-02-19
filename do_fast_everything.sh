3ecf8b1b198a421cbf252d13296ce2fc9fe9695e
Reynier Cruz Torres <reynier@mit.edu>
2018-02-19 16:51:28 -0500

    Started removinf unnecessary files from top dir There are many files that are not used in the code either because we now use a newer version, or because they were never used in the first place, and they are just floating there. So I removed these files from the top directory and started moving them to the directory named "obsolete". I don't want to get rid of them altogether. I want to keep a copy in case I decide to use some of these files in the future.

--- do_fast_everything.sh
+++
@@ -1,4 +0,0 @@
-source do_helium_fast_benhar.sh
-source do_helium_fast_kaptari.sh
-source do_tritium_fast_benhar.sh
-source do_tritium_fast_kaptari.sh
--- do_tritium_fast_kaptari.sh
+++
@@ -1,6 +0,0 @@
-cp infiles/Reynier/3H_fast_1day_rad_kaptari_cont.data infiles/current.data
-./simc
-python python/make_root_one_default.py
-cd output/
-mv current.root fast_H_kaptari_cont.root 
-cd ..
